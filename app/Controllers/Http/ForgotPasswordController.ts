9474eed9fc8fff1df70961dc6b197e4df12e00f1
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-28 13:55:58 -0300

    feat: send email

--- app/Controllers/Http/ForgotPasswordController.ts
+++ app/Controllers/Http/ForgotPasswordController.ts
@@ -1,6 +1,10 @@
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { DateTime } from "luxon";
 import Hash from "@ioc:Adonis/Core/Hash";
+import { string } from '@ioc:Adonis/Core/Helpers'
+
+
+import Mail from "@ioc:Adonis/Addons/Mail";
 
 import User from "App/Models/User";
 
@@ -10,16 +14,25 @@ export default class ForgotPasswordController {
       const email = request.input("email");
       const user = await User.findByOrFail("email", email);
 
-      user!.token = await Hash.make(user!.email);
+      // user!.token = await Hash.make(user!.email);
+      user!.token = await string.generateRandom(10).toString();
       user!.token_created_at = DateTime.local();
 
       await user?.save();
+
+      await Mail.send((message) => {
+        message
+          .htmlView("emails/forgot_password", {
+            email: email,
+            token: user!.token,
+            link: ``
+          })
+          .to(user.email)
+          .from("martinsgabriel1956@gmail.com", "Gabriel Martins")
+          .subject("Reset Password");
+      });
     } catch (err) {
-      return response 
-        .status(err.status)
-        .send({
-          error: { message: "Algo não deu certo, esse e-mail existe?" },
-        });
+      return response.badRequest(err.message);
     }
   }
 }
